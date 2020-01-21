8027c9e01ec489895c266e5e2398d749fe9de09f
Fly-Away <soundgraphia@gmail.com>
2020-01-21 10:51:28 +0700

    First commit

---
+++ src/main/java/com/movie/db/KotakKantorMovieDB/KotakKantorMovieDbApplication.java
@@ -0,0 +1,13 @@
+package com.movie.db.KotakKantorMovieDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotakKantorMovieDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KotakKantorMovieDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movie/db/KotakKantorMovieDB/KotakKantorMovieDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.db.KotakKantorMovieDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KotakKantorMovieDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
