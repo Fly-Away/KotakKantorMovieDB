cc8cf97cf007a54da63528ec6634eaaa6ca76b0d
Fly-Away <soundgraphia@gmail.com>
2020-01-27 15:51:26 +0700

    menambahkan RepositoryRestResource untuk auto CRUD
    menambahkan RepositoryRestResource untuk mendukung JPA dan RESTful Api HATEOAS

--- src/main/java/com/movie/db/KotakKantorMovieDB/repository/CustomerRepository.java
+++ src/main/java/com/movie/db/KotakKantorMovieDB/repository/CustomerRepository.java
@@ -5,10 +5,14 @@ import java.util.List;
 import com.movie.db.KotakKantorMovieDB.models.Customer;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
 public interface CustomerRepository extends MongoRepository<Customer, String> {
     
     public Customer findByFirstName(String firstName);
-    public List<Customer> findByLastName(String lastName);
+    // public List<Customer> findByLastName(String lastName);
+    List<Customer> findByLastName(@Param("name") String name);
 
 }
